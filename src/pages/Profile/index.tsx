2815504f0fdede722196cb771b3abee5d3fd2061
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2020-08-11 21:49:35 -0300

    informações do usuário

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles'
+
+import ProfileData from '../../components/ProfileData'
 
 const Profile: React.FC = () => {
-  return <div />
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'gabriel-camargo'}
+            name={'Gabriel Camargo'}
+            avatarUrl={'https://avatars2.githubusercontent.com/u/37885082?v=4'}
+            followers={666}
+            following={50}
+            company={'Apus'}
+            location={'Pindamonhangaba, Brazil'}
+            email={'gabriel.rcamargo@outlook.com'}
+            blog={'gabriel.dev.br'}
+          />
+        </LeftSide>
+
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  )
 }
 
 export default Profile
