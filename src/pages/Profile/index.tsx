5348c63de48c4ac0128998ccbf5e4c0924605bc9
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2020-08-12 22:14:43 -0300

    lista de repos

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import { Container, Main, LeftSide, RightSide } from './styles'
+import { Container, Main, LeftSide, RightSide, Repos } from './styles'
 
 import ProfileData from '../../components/ProfileData'
+import RepoCard from '../../components/RepoCard'
 
 const Profile: React.FC = () => {
   return (
@@ -22,7 +23,25 @@ const Profile: React.FC = () => {
           />
         </LeftSide>
 
-        <RightSide></RightSide>
+        <RightSide>
+          <Repos>
+            <h2>Random Repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <RepoCard
+                  key={n}
+                  username={'gabriel-camargo'}
+                  reponame={'youtube-content'}
+                  description={'Lorem ipsum dolor sit amet'}
+                  language={n % 3 === 0 ? 'JavaScript' : 'Typescript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
       </Main>
     </Container>
   )
